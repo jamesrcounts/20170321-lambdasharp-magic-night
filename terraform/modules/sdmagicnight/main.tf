7acfb770be19ec7584ca30c928beaae11ae8017e
Dylan Vaughn <dylancvaughn@gmail.com>
2017-03-21 19:36:23 -0700

    wip lambda function

--- terraform/modules/sdmagicnight/main.tf
+++ terraform/modules/sdmagicnight/main.tf
@@ -6,3 +6,9 @@ resource "aws_cloudformation_stack" "upload_bucket" {
   template_body = "${file("${path.module}/upload_bucket.yaml")}"
   on_failure = "DELETE"
 }
+
+resource "aws_cloudformation_stack" "lambda_function" {
+  name = "${var.name}-${var.env}-lambda-function-stack"
+  template_body = "${file("${path.module}/lambda.yaml")}"
+  on_failure = "DELETE"
+}
