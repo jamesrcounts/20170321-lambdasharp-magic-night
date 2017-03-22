f60b0fe5ac96f927f0f095e4f0b95b868c036d37
Dylan Vaughn <dylancvaughn@gmail.com>
2017-03-21 21:08:24 -0700

    wip lambda stuff

--- terraform/modules/sdmagicnight/main.tf
+++ terraform/modules/sdmagicnight/main.tf
@@ -7,20 +7,14 @@ data "archive_file" "lambda_zip" {
   type = "zip"
 }
 
-resource "aws_cloudformation_stack" "upload_bucket" {
-  name = "${var.name}-${var.env}-upload-bucket-stack"
-  template_body = "${file("${path.module}/upload_bucket.yaml")}"
-  on_failure = "DELETE"
-}
-
 resource "aws_cloudformation_stack" "lambda_bucket" {
   name = "${var.name}-${var.env}-lambda-bucket-stack"
-  template_body = "${file("${path.module}/upload_bucket.yaml")}"
+  template_body = "${file("${path.module}/lambda_bucket.yaml")}"
   on_failure = "DELETE"
 }
 
 resource "aws_s3_bucket_object" "lambda_zip" {
-  bucket = "${aws_cloudformation_stack.upload_bucket.outputs["UploadBucket"]}"
+  bucket = "${aws_cloudformation_stack.lambda_bucket.outputs["UploadBucket"]}"
   depends_on = ["data.archive_file.lambda_zip"]
   etag = "${md5(file("../../../tmp/lambda.zip"))}"
   key = "lambda.zip"
@@ -32,7 +26,16 @@ resource "aws_cloudformation_stack" "lambda_function" {
   name = "${var.name}-${var.env}-lambda-function-stack"
   template_body = "${file("${path.module}/lambda.yaml")}"
   parameters {
-    LambdaBucket = "${aws_cloudformation_stack.upload_bucket.outputs["UploadBucket"]}"
+    LambdaBucket = "${aws_cloudformation_stack.lambda_bucket.outputs["UploadBucket"]}"
+  }
+  on_failure = "DELETE"
+}
+
+resource "aws_cloudformation_stack" "upload_bucket" {
+  name = "${var.name}-${var.env}-upload-bucket-stack"
+  template_body = "${file("${path.module}/upload_bucket.yaml")}"
+  parameters {
+    LambdaFunctionArn = "${aws_cloudformation_stack.lambda_function.outputs["LambdaFunctionArn"]}"
   }
   on_failure = "DELETE"
 }
